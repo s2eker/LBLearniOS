fc23a1ab4f333a5cff8523d7f71b89faee2ae930
libing <294842586@qq.com>
2019-01-22 14:45:11 +0800

    ADDED: 初始化工程

---
+++ LBLearniOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LBLearniOS
+//
+//  Created by 李兵 on 2019/1/22.
+//  Copyright © 2019 李兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LBLearniOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LBLearniOS
+//
+//  Created by 李兵 on 2019/1/22.
+//  Copyright © 2019 李兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
